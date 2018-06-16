324df0b56a44b0ab0c32ce5055fdd40c2cf5b34f
unknown <hyesu_1215@naver.com>
2018-06-16 23:05:32 +0900

    first version

---
+++ src/A.java
@@ -0,0 +1,9 @@
+public class A { 
+ 	public static void main(String[] args) { 
+ 		System.out.println("2018-06-16"); 
+ 		B b = new B("*******************"); 
+		b.println(); 
+ 		C c = new C("Today!"); 
+ 		b.show(); 
+ 	} 
+ } 
---
+++ src/B.java
@@ -0,0 +1,12 @@
+public class B { 
+	String string; 
+ 	public B() { 
+ 		this.string = "*0*"; 
+ 	} 
+ 	public B(String string) { 
+ 		this.string = str; 
+ 	} 
+ 	public void println() { 
+		System.out.println(string); 
+	} 
+} 
